64c2fafacdf087708ea070e4ddb942b531de004c
MarcSerraPeralta <marcserraperalta@gmail.com>
2026-01-18 11:28:37 +0100

    [config] Add disk usage alert rules in Grafana

--- config_files/monitoring/log_disk_usage.sh
+++ config_files/monitoring/log_disk_usage.sh
@@ -2,9 +2,10 @@
 LOGFILE="/home/marc/monitoring/data/disk_use.csv"
 
 for i in {1..6}; do
-  DISK_USAGE=$(df -k / | awk 'NR==2 {print $3 "," $4}')
-  
-  printf "used,available\n$DISK_USAGE" > "$LOGFILE"
-  
+  DISK_USAGE=$(df -k / | awk 'NR==2 {print $3 "," $4 "," $3 / $4 }')
+  DISK_USAGE_SRV=$(df -k /srv/ | awk 'NR==2 {print $3 "," $4, "," $3 / $4 }')
+
+  printf "used,available,used_fraction,used_srv,available_srv,used_fraction_srv\n$DISK_USAGE,$DISK_USAGE_SRV" > "$LOGFILE"
+
   sleep 10
 done
